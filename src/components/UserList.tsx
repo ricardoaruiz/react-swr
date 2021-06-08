14164eb6196cca93b10dccfd0a752d20f95ff7bd
Ricardo Ruiz <ricardo.ruiz.ext@dasa.com.br>
2021-06-08 11:01:03 -0300

    Mutate data and refresh cache

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { mutate as mutateGlobal } from 'swr'
 
 import useFetch from 'hooks/useFetch'
+import api from 'services/api'
 
 type User = {
   id: number
@@ -9,17 +11,36 @@ type User = {
 }
 
 export const UserList = () => {
+  const { data: users, mutate } = useFetch<User[]>('users')
 
-  const { data } = useFetch<User[]>('users')
+  const handleChangeName = React.useCallback((user: User) => {
+    api.put(`users/${user.id}`, { name: 'Teobaldo' })
 
-  if (!data) {
+    const newUserList = users?.map(currentUser => {
+      if(user.id === currentUser.id) {
+        return { ...currentUser, name: 'Teobaldo' }
+      }
+      return currentUser
+    })
+
+    // Faz a mudança do cache local
+    mutate(newUserList)
+
+    // Faz a mudança do cache global para todas as requisiçãoes com a url informada
+    mutateGlobal(`users/${user.id}`)
+  }, [mutate, users])
+
+  if (!users) {
     return <div>Carregando...</div>
   }
 
   return (
     <ul>
-      {data.map(({id, name}) => (
-        <li key={id}><Link to={`/${id}`}>{name}</Link></li>
+      {users.map((user) => (
+        <li key={user.id}>
+          <Link to={`/${user.id}`}>{user.name}</Link>
+          <button type="button" onClick={() => handleChangeName(user)}>Altera nome</button>
+        </li>
       ))}
     </ul>
   )
--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -3,12 +3,12 @@ import api from 'services/api'
 
 export function useFetch<Data = any>(url: string) {
 
-  const { data, error } = useSWR<Data>(url, async url => {
+  const { data, error, mutate } = useSWR<Data>(url, async url => {
     const response = await api.get<Data>(url)
     return response.data
   })
 
-  return { data, error }
+  return { data, error, mutate }
 }
 
 export default useFetch
